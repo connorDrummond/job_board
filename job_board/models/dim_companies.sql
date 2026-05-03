a958380b3db1bfe2c1816bf2a1aefb652fe815d3
cdrummond31696@gmail.com <cdrummond31696@gmail.com>
2026-05-03 20:32:33 +0000

    1`

--- job_board/models/dim_companies.sql
+++ job_board/models/dim_companies.sql
@@ -4,6 +4,6 @@ company,
 location,
 company_url,
 max(company_num_employees) as company_num_employees
- from postgres.bronze.jobs
+ from {{ source('bronze', 'jobs') }}
  where company is not null
  group by 1,2,3,4
